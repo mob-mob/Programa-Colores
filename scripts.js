518c882d39df9bd5eeaf151b22c16596e7b50c6a
mob-mob <nerev_@outlook.com.com>
2023-11-22 15:25:47 -0800

    Programa colores 2

--- scripts.js
+++ scripts.js
@@ -1,33 +1,83 @@
+let la = new RegExp("(^a|^A)");
+let le = new RegExp("(^e|^E)");
+let li = new RegExp("(^i|^I)");
+let lo = new RegExp("(^o|^O)");
+let lu = new RegExp("(^u|^U)");
+let ensaje = new RegExp(/ensaje\([^()]*\)$/i);
+let inicio = new RegExp(/inicio$/i);
+let out = new RegExp(/out$/i);
+
 input = document.querySelector(".input");
+contenedor = document.querySelector(".contenedor");
 parrafo = document.querySelector(".parrafo");
-span = document.querySelector(".span");
+boton = document.querySelector(".botonD");
 
-input.addEventListener("input", leerinput);
+input.addEventListener("input", leerInput);
+boton.addEventListener("click", validar);
 
-function leerinput(e) {
-    if (e.target.value.charAt(0) === "a" || e.target.value.charAt(0) === "A") {
-        span.style.color = "#50fa7b";
-    } else if (e.target.value.charAt(0) === "e" || e.target.value.charAt(0) === "E") {
-        span.style.color = "#ffb86c";
-    } else if (e.target.value.charAt(0) === "i" || e.target.value.charAt(0) === "I") {
-        span.style.color = "#ff79c6";
-    } else if (e.target.value.charAt(0) === "o" || e.target.value.charAt(0) === "O") {
-        span.style.color = "#bd93f9";
-    } else if (e.target.value.charAt(0) === "u" || e.target.value.charAt(0) === "U") {
-        span.style.color = "#ff5555";
+function leerInput(e) {
+
+    if (la.test(e.target.value)) {
+        parrafo.style.color = "#50fa7b";
+    } else if (le.test(e.target.value)) {
+        parrafo.style.color = "#ffb86c";
+    } else if (li.test(e.target.value)) {
+        parrafo.style.color = "#ff79c6";
+    } else if (lo.test(e.target.value)) {
+        parrafo.style.color = "#bd93f9";
+    } else if (lu.test(e.target.value)) {
+        parrafo.style.color = "#ff5555";
     } else {
-        span.style.color = "#f8f8f2";
+        parrafo.style.color = "#f8f8f2";
     }
+    //parentesis(e);
     corte(e);
-    span.textContent = " " + e.target.value;
+    parrafo.textContent = e.target.value;
 }
 
 function corte(e) {
     if (e.target.value.charAt(e.target.value.length - 1) === "#") {
-        nspan = document.createElement("span");
-        parrafo.appendChild(nspan);
-        span = nspan;
+        nParrafo = document.createElement("p");
+        nParrafo.classList.add("parrafo")
+        contenedor.appendChild(nParrafo);
+        parrafo = nParrafo;
         e.target.value = "";
     }
 }
 
+/*function parentesis(e) {
+    if (e.target.value.charAt(e.target.value.length - 1) === "(") {
+        nspan = document.createElement("span");
+        parrafo.appendChild(nspan);
+        parrafo.textContent += "(";
+        parrafo = nspan;
+        e.target.value = "";
+    }
+}*/
+
+function validar(e) {
+    let m = "";
+    let c = 0;
+    if (inicio.test(contenedor.children[0].textContent)) {
+        c++;
+        for (let i = 1; ensaje.test(contenedor.children[i].textContent); i++) {
+            if (ensaje.test(contenedor.children[i].textContent)) {
+                console.log("Si");
+                m += contenedor.children[i].textContent.substring(7).replace(")", "") + "\n";
+                c++;
+            }
+        }
+        if (out.test(contenedor.children[contenedor.children.length - 2].textContent)) {
+            c++;
+        } else {
+            c = 0;
+        }
+    }
+
+    console.log(c);
+    if (c >= 3) {
+        alert(m);
+    } else {
+        alert("Está mal");
+    }
+}
